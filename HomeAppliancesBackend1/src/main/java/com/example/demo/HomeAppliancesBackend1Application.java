40af502dca7e1a9a2bf30ec6f2653df723d5b7ae
Dhanashri Navsar <dbnavsar2018@gmail.com>
2023-04-12 17:41:19 +0530

    ProjectFinal

---
+++ HomeAppliancesBackend1/src/main/java/com/example/demo/HomeAppliancesBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAppliancesBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAppliancesBackend1Application.class, args);
+	}
+
+}
